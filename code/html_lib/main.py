c9b9539c60408796c58d9c85c6b1db5ed6656bb7
みねぎしれい <68362169+minegishirei@users.noreply.github.com>
2024-04-18 19:38:40 +0900

    save

--- code/html_lib/main.py
+++ code/html_lib/main.py
@@ -1,7 +1,53 @@
 from jinja2 import Template
 html = '''
 
-## test
+
+<h2>{{ name }}の評価</h2>
+
+<canvas id="abilityChart"></canvas>
+
+
+<script>
+    var ctx = document.getElementById("abilityChart");
+    var myRadarChart = new Chart(ctx, {
+        type: 'radar', 
+        data: { 
+            labels: ["英語", "数学", "国語", "理科", "社会"],
+            datasets: [{
+                label: 'Aさん',
+                data: [92, 72, 86, 74, 86],
+                backgroundColor: 'RGBA(225,95,150, 0.5)',
+                borderColor: 'RGBA(225,95,150, 1)',
+                borderWidth: 1,
+                pointBackgroundColor: 'RGB(46,106,177)'
+            }, {
+                label: 'Bさん',
+                data: [73, 95, 80, 87, 79],
+                backgroundColor: 'RGBA(115,255,25, 0.5)',
+                borderColor: 'RGBA(115,255,25, 1)',
+                borderWidth: 1,
+                pointBackgroundColor: 'RGB(46,106,177)'
+            }]
+        },
+        options: {
+            title: {
+                display: true,
+                text: '試験成績'
+            },
+            scale:{
+                ticks:{
+                    suggestedMin: 0,
+                    suggestedMax: 100,
+                    stepSize: 10,
+                    callback: function(value, index, values){
+                        return  value +  '点'
+                    }
+                }
+            }
+        }
+    });
+</script>
+
 
 <h2>{{ name }}の年収分布</h2>
 
