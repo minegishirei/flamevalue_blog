11f2465bb3e25826614770af57ab2be86e92ef9a
みねぎしれい <68362169+minegishirei@users.noreply.github.com>
2024-04-18 10:42:23 +0900

    save

--- code/html_lib/main.py
+++ code/html_lib/main.py
@@ -23,12 +23,14 @@ function DrawChart(labels, data1, data2) {
 	  data: {
 	    labels: labels,
 	    datasets: [{
-	      label: 'VDOT40',
+	      label: '下限年収',
 	      data: data1,
+			backgroundColor: '#9BD0F5',
 	      borderColor: '#f88',
 	    }, {
-	      label: 'VDOT50',
+	      label: '上限年収',
 	      data: data2,
+		  backgroundColor: '#FFB1C1',
 	      borderColor: '#484',
 	    }]
 	  },
